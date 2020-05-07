14c2399c3beaa15a4f36821364fa624806eb72f6
Katie Durand <khd25@cornell.edu>
2020-05-07 15:01:08 -0500

    example 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Big-O-Time-Review
+# Cracking the Coding Interview
+
+## Chapter 6: Big O Time
+
+### This repo is where I have saved my work while progressing through Chapter 6 of Cracking the Coding Interview.
+
+These problems are not part of the 189 advertised interview prep problems, but are the examples from the Big O Time and Space Complexity chapter.
+
+I have taken all examples (originally written in Java) from this chapter, re-worked them into JavaScript, solved them, and included comments on their respective Big O times.
---
+++ problems.js
@@ -0,0 +1,37 @@
+/* 
+
+BIG O TIME - THE CHAPTER FROM CRACKING THE CODING INTERVIEW
+
+I have "translated" the code examples from Java to JavaScript where possible 
+
+These were all tested on repl.it (hence the console.logs everywhere)
+please see: https://repl.it/@khd25/CtCI-Big-O-Chapter-Examples to run them more easily!
+
+====================================================================
+
+Example 0:  O(2^N) Time
+
+Generally when you have a recursive function that makes multiple calls, the runtime will often (but not always) look like: O(branches^depth), where branches is the number of times each recursive call branches. in this case, the example is O(2^N)
+
+*/
+
+const exampleOf2ToTheNTime = (num) => {
+  if (num <= 1) {
+    return 1;
+  }
+  return exampleOf2ToTheNTime(num - 1) + exampleOf2ToTheNTime(num - 1);
+};
+
+console.log('example of 2^N time: ', exampleOf2ToTheNTime(4));
+
+//                     (4)
+//                   returns
+//             (3)      +      (3)
+//           returns         returns
+//         (2)  +  (2)  +  (2)  +  (2)
+//       returns returns returns returns
+// base  (1)+(1)+(1)+(1)+(1)+(1)+(1)+(1) = 8
+
+// returns 8
+
+//=================================================================
