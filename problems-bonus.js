350adb0b4e62d0736e90da206af452838be2abb2
Katie Durand <khd25@cornell.edu>
2020-05-11 11:25:28 -0500

    example 3 & 4

--- problems-bonus.js
+++ problems-bonus.js
@@ -42,3 +42,59 @@ function power(a, b) {
 console.log('power: ', power(2, 3)); // 8
 
 //=================================================================
+
+/* 
+
+BONUS Example 3: Constant Time - Modulus Operator
+
+The following computes a % b (a modulus b). Runtime?
+
+How to calculate modulus (i.e. remainder)
+x - y * Math.floor(x / y)
+
+9 - 5 = 4
+4 * (Math.floor(9 / 5)) 
+9 / 5 = 1.8
+Math.floor(1.8) = 1
+1 * 4 = 4
+9 % 5 = 4
+
+*/
+
+function modulus(a, b) {
+  if (b <= 0) {
+    return -1;
+  }
+  let result = a - b * Math.floor(a / b);
+  return result;
+}
+
+console.log('modulus%: ', modulus(9, 5));
+
+//=================================================================
+
+/* 
+
+BONUS Example 4: O(N/M) Time - Simple Division
+
+The following performs integer division, assume nums are positive.
+Runtime?
+
+O(a/b)
+
+*/
+
+function div(a, b) {
+  6 / 2;
+  let count = 0;
+  let sum = b;
+  while (sum <= a) {
+    sum += b;
+    count++;
+  }
+  return count;
+}
+
+console.log('division: ', div(33, 11)); // 3
+
+//=================================================================
